67f85b1d54c6ffdfee9b81a6c46b6642a3128a5b
danmulonzi001 <danmulonzi001@gmail.com>
2020-04-10 07:40:58 +0300

    Initial Commit

---
+++ voting.js
@@ -0,0 +1,14 @@
+
+    let loginButton = document.getElementById("loginB")
+    loginButton.addEventListener('click', login)
+
+    let cancel = document.getElementsByClassName("cancelbtn")[0]
+        cancel.addEventListener('click', function(){
+        console.log("cancelled");
+
+    })
+
+    function login(){
+        console.log("looged in")   
+        window.open("login.html", "_self")
+    }
