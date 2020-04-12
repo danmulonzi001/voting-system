60a55e2227e2b15bde4cd186d04a7ffa244828c7
danmulonzi001 <danmulonzi001@gmail.com>
2020-04-12 03:41:23 +0300

    updated

---
+++ password.js
@@ -0,0 +1,6 @@
+// let submitbutton=document.getElementById("submit");
+// submit.addEventListener('click',login());
+// function login(){
+//     console.log("looged in")   
+//     window.open("index.html", "_self")
+// }
--- voting.js
+++ voting.js
@@ -2,6 +2,22 @@
     let loginButton = document.getElementById("loginB")
     loginButton.addEventListener('click', login)
 
+    { 
+        }
+    {
+        var empt = document.inputid["loginB"]["submit"].value;
+        if (empt == "")
+        {
+        alert("Please input a Value");
+        return false;
+        }
+        else 
+        {
+        alert('Code has accepted : you can try another');
+        return true; 
+        }
+        }
+    
     let cancel = document.getElementsByClassName("cancelbtn")[0]
         cancel.addEventListener('click', function(){
         console.log("cancelled");
@@ -11,4 +27,5 @@
     function login(){
         console.log("looged in")   
         window.open("login.html", "_self")
+    
     }
