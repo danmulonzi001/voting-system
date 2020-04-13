a4130901b90e7afbe2ccb4f6ca475d713b91e1ec
danmulonzi001 <danmulonzi001@gmail.com>
2020-04-13 05:24:37 +0300

    -update

--- password.js
+++
@@ -1,6 +0,0 @@
-// let submitbutton=document.getElementById("submit");
-// submit.addEventListener('click',login());
-// function login(){
-//     console.log("looged in")   
-//     window.open("index.html", "_self")
-// }
--- voting.js
+++ voting.js
@@ -2,21 +2,6 @@
     let loginButton = document.getElementById("loginB")
     loginButton.addEventListener('click', login)
 
-    { 
-        }
-    {
-        var empt = document.inputid["loginB"]["submit"].value;
-        if (empt == "")
-        {
-        alert("Please input a Value");
-        return false;
-        }
-        else 
-        {
-        alert('Code has accepted : you can try another');
-        return true; 
-        }
-        }
     
     let cancel = document.getElementsByClassName("cancelbtn")[0]
         cancel.addEventListener('click', function(){
@@ -27,5 +12,9 @@
     function login(){
         console.log("looged in")   
         window.open("login.html", "_self")
+
     
+    }
+    function submit(){
+        console.log("do not login when password is not entered")
     }
